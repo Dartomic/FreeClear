fc9196d66598c5e7a87f68f017d6d152b1452aba
Dartomic <dartomictech@gmail.com>
2021-10-19 16:48:32 -0400

    Start source port

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "methods.h"
+
+/* Replace usage */
+// static List<int> ToStudy = new List<int>();
+// static List<TopicModel> TopicsList = new List<TopicModel>();
+// static List<TopicModel> topics = new List<TopicModel>();
+// static Globals globals = new Globals();
+
+int main(void)
+{
+    GetTheDate();
+    GetPath();
+    StartUp();
+}
+
+
